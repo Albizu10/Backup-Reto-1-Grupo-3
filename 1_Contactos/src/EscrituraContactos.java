d2f66fb8d12e8301578c2e997ece1bba16f505ed
Malen <ikmaierbe24@lhusurbil.eus>
2025-10-27 09:54:45 +0100

    escritura con formato estructurado (sin patrón de validación)

--- 1_Contactos/src/EscrituraContactos.java
+++ 1_Contactos/src/EscrituraContactos.java
@@ -2,11 +2,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import java.io.IOException;
 
 public class EscrituraContactos {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         try {
             File ruta = new File("contactos.csv");
@@ -17,28 +23,54 @@ public class EscrituraContactos {
 
             String nombre, apellido, correo, ciudad, pais;
             int telefono;
+            boolean seguirEscribiendo = true;
 
-            System.out.println("Nombre: ");
-            nombre = sc.nextLine();
+            System.out.println(ANSI_BLUE + "\nESCRITURA DE NUEVOS CONTACTOS \n------------------------------------\n" + ANSI_RESET);
 
-            System.out.println("Apellido: ");
-            apellido = sc.nextLine();
+            while (seguirEscribiendo == true) {
+                System.out.println(ANSI_BLUE + "1) Escribir \n2) Salir\n" + ANSI_RESET);
 
-            System.out.println("Correo electrónico: ");
-            correo = sc.nextLine();
+                try{
+                    int opcion = sc.nextInt();
+                    sc.nextLine();
 
-            System.out.println("Teléfono: ");
-            telefono = sc.nextInt();
-            sc.nextLine();
+                    if (opcion == 1) {
+                        System.out.println("\nNombre: ");
+                        nombre = sc.nextLine();
 
-            System.out.println("Ciudad: ");
-            ciudad = sc.nextLine();
+                        System.out.println("\nApellido: ");
+                        apellido = sc.nextLine();
 
-            System.out.println("País:");
-            pais = sc.nextLine();
+                        System.out.println("\nCorreo electrónico: ");
+                        correo = sc.nextLine();
+                        
+                        System.out.println("\nTeléfono: ");
+                        telefono = sc.nextInt();
+                        sc.nextLine();
 
-            bfw.write("\n\"" + nombre + " " + apellido + "\",\"" + correo + "\",\"" + telefono +  "\",\"" + ciudad + "\",\"" + pais + "\"");
-            
+                        System.out.println("\nCiudad: ");
+                        ciudad = sc.nextLine();
+
+                        System.out.println("\nPaís:");
+                        pais = sc.nextLine();
+
+                        bfw.write("\n\"" + nombre + " " + apellido + "\",\"" + correo + "\",\"" + telefono + "\",\""
+                                + ciudad + "\",\"" + pais + "\"");
+
+                        System.out.println( ANSI_GREEN + "\n----------------------------------------------------\n\nLa escritura del contacto "
+                                        + nombre + " " + apellido + "se ha realizado correctamente. \n\n----------------------------------------------------\n" + ANSI_RESET);
+                    } else if (opcion == 2) {
+                        seguirEscribiendo = false;
+                        System.out.println(ANSI_BLUE + "\n------------------------------------\nSaliendo...\n" + ANSI_RESET);
+                    } else {
+                        System.out.println(
+                                ANSI_RED + "\nEl número introducido no es correcto, escriba 1 o 2 para indicar la acción que quiere realizar\n" + ANSI_RESET);
+                    } 
+                } catch (InputMismatchException e){
+                    System.out.println(ANSI_RED + "\nEl formato de dato introducido no es correcto, escriba un número, por favor\n" + ANSI_RESET);
+                    sc.nextLine();
+                }
+            }
             bfw.close();
             sc.close();
         } catch (FileNotFoundException e) {
