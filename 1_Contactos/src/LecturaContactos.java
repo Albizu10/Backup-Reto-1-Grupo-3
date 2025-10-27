187cc49effc4f4ac622b9f826521a493283d0d67
Malen <ikmaierbe24@lhusurbil.eus>
2025-10-27 09:39:19 +0100

    lectura con formato estructurado

--- 1_Contactos/src/LecturaContactos.java
+++ 1_Contactos/src/LecturaContactos.java
@@ -5,6 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class LecturaContactos {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
         try {
             File ruta = new File("contactos.csv");
@@ -13,17 +18,43 @@ public class LecturaContactos {
 
             String linea;
 
-            while ((linea = bfr.readLine()) != null) {
-                System.out.println(linea);
-            }
+            // Saltar la primera línea porque es el encabezado
+            bfr.readLine();
 
+            System.out.println(ANSI_BLUE + "\nLEYENDO CONTACTOS DEL FICHERO '" + ruta + "'" + ANSI_RESET);
+            System.out.println(ANSI_BLUE + "----------------------------------------------\n" + ANSI_RESET);
+
+            while ((linea = bfr.readLine()) != null) {
+                String lineaSinSeparador = linea.replace("\"", "");
+
+                String[] campos = lineaSinSeparador.split(",");
+
+                if (campos.length >= 5) {
+                    String nombreCompleto = campos[0];
+                    String correo = campos[1];
+                    String telefono = campos[2];
+                    String ciudad = campos[3];
+                    String pais = campos[4];
+
+                    System.out.println("Nombre completo: " + nombreCompleto);
+                    System.out.println("Correo: " + correo);
+                    System.out.println("Teléfono: " + telefono);
+                    System.out.println("Ciudad: " + ciudad);
+                    System.out.println("País: " + pais);
+                    System.out.println(ANSI_GREEN + "\n-----------------------------------\n" + ANSI_RESET);
+
+                } else {
+                    System.out.println(ANSI_RED + "Línea con formato incorrecto: \n" + linea + ANSI_RESET);
+                    System.out.println(ANSI_RED + "\n-----------------------------------\n" + ANSI_RESET);
+                }
+
+            }
             bfr.close();
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
 
     }
 }
