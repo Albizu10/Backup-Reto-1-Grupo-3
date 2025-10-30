ab023f5f3d0cebe7703f427effc5da69bb2a065d
Malen <ikmaierbe24@lhusurbil.eus>
2025-10-30 08:53:21 +0100

    Escritura de contactos en XML

--- 4_XML/src/EscrituraXML.java
+++ 4_XML/src/EscrituraXML.java
@@ -1,28 +1,84 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Attr;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import java.io.File;
+
 public class EscrituraXML {
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
-        
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.newDocument();
+
+            File ruta = new File("contactosEscritura.xml");
+            StreamResult sr = new StreamResult(ruta);
+
+            String[] arrayNomCompl = { "Ada Martinez", "Administrator", "Carla Jimenez", "Comercializadora Ruiz S.A.", "Juan Martinez", "Laura Hernandez" };
+            String[] arrayCorreos = { "adamartinez@techsolutuion.com", "ikmssaid24@lhusurbil.eus", "carlajimenez@techsolutuion.com", "ruiz@gmail.com", "juanmartinez@techsolutuion.com", "laurahernandez@gmail.com" };
+            String[] arrayTelefonos = { "555-0101", "245-345", "555-0102", "5664-7631", "555-0103", "6234-8909" };
+            String[] arrayCiudades = { "Madrid", "Madrid", "Santander", "Quito", "Soria", "Albacete" };
+            String[] arrayPaises = { "España", "España", "España", "España", "España", "España" };
+
+            System.out.println("\nGenerando XML con datos de contacto...");
+
+            Element contactos = doc.createElement("Contactos");
+            doc.appendChild(contactos);
+
+            for (int i = 0; i < arrayNomCompl.length; i++) {
+                Element contacto = doc.createElement("Contacto");
+                Attr id = doc.createAttribute("id");
+                contacto.setAttribute("id", String.valueOf(i + 1));
+                contactos.appendChild(contacto);
+
+                Element nombreCompleto = doc.createElement("NombreCompleto");
+                contacto.appendChild(nombreCompleto);
+                nombreCompleto.setTextContent(arrayNomCompl[i]);
+
+                Element correoElectronico = doc.createElement("correoElectronico");
+                contacto.appendChild(correoElectronico);
+                correoElectronico.setTextContent(arrayCorreos[i]);
+
+                Element telefono = doc.createElement("Telefono");
+                contacto.appendChild(telefono);
+                telefono.setTextContent(arrayTelefonos[i]);
+
+                Element ciudad = doc.createElement("Ciudad");
+                contacto.appendChild(ciudad);
+                ciudad.setTextContent(arrayCiudades[i]);
+
+                Element pais = doc.createElement("Pais");
+                contacto.appendChild(pais);
+                pais.setTextContent(arrayPaises[i]);
+            }
+
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transf = tf.newTransformer();
+            DOMSource doms = new DOMSource(doc);
+
+            transf.transform(doms, sr);
+
+            System.out.println(ANSI_GREEN + "\n-----------------------------------\n" + ANSI_RESET);
+            System.out.println(ANSI_GREEN + "Se ha generado el archivo '" + ruta + "' con " + arrayNomCompl.length + " datos de contacto\n" + ANSI_RESET);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
     }
 }
-
-/*
-Hemos recibido una llamada en TechSolutions S.L. y nos comunican que un organismo
-estatal ha tenido un problema que atañe a la seguridad en IT y por una orden judicial
-en la que nos vemos envueltos de forma colateral, quieren consultar nuestros datos.
-Este ente judicial trabaja con ficheros XML (y usan algún atributo al menos en estos
-ficheros). Nuestro objetivo es mostrar transparencia y decidiremos enviarles un
-programa que trabaja con los contactos. Nuestro objetivo es mostrar que hacemos las
-cosas bien en nuestra empresa, somos legales y para ello crearemos un programa que
-cree un fichero XML con la estructura correspondiente. Posteriormente haremos la
-lectura de dicho fichero XML. Los datos se mostrarán en terminal cuidando la
-estructura y el estilo lo máximo posible en terminal de manera que sea fácil de leer.
-Con estos datos, el ente judicial hará su investigación correspondiente y nosotros
-seguiremos tranquilos.
-
-Desde el ente judicial nos vuelven a contactar y nos indican a TechSolutions S.L. que
-sobre el fichero XML anterior hagamos 3 consultas que consideres interesantes dentro
-de ese DOM, facilitandoles así la búsqueda de la información. Es posible que hayan
-detectado alguna ip origen y de ahí quieran contrastar datos por posible irregularidad
-en seguridad, con lo que cualquier consulta en este ámbito les podría valer. Los datos
-se mostrarán en terminal cuidando la estructura y el estilo lo máximo posible en
-terminal de manera que se lea fácil leer.
- */
