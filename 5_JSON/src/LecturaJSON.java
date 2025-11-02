0f29238233938c1609653cd0662bc5bbcac4129d
Malen <ikmaierbe24@lhusurbil.eus>
2025-11-02 19:09:39 +0100

    Lectura y escritura de contactos JSON documentada

--- 5_JSON/src/LecturaJSON.java
+++ 5_JSON/src/LecturaJSON.java
@@ -8,15 +8,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
-
-import java.util.Map;
 import java.util.Iterator;
+import java.util.Map;
 
+/**
+ * Clase que permite leer y mostrar en consola los contactos almacenados en el archivo JSON 'contactos.json'.
+ * Muestra los datos con formato claro y legible en la terminal.
+ * 
+ * @version 1.0
+ */
 public class LecturaJSON {
-    public static String ANSI_GREEN = "\u001B[32m";
-    public static String ANSI_BLUE = "\u001B[34m";
-    public static String ANSI_RESET = "\u001B[0m";
+    /**Código ANSI para restablecer el color de la terminal */
+    public static final String ANSI_RESET = "\u001B[0m";
+    /**Código ANSI para imprimir los resultados en color verde en la terminal */
+    public static final String ANSI_GREEN = "\u001B[32m";
+    /**Código ANSI para imprimir mensajes de cabecera en color cyan en la terminal */
+    public static final String ANSI_CYAN = "\u001B[36m";
+    /**Código ANSI para imprimir errores en color rojo en la terminal */
+    public static final String ANSI_RED = "\u001B[31m";
 
+    /**
+     * Método principal del programa. 
+     * Permite leer el archivo JSON y mostrar cada contacto en formato estructurado.
+     * @param args
+     */
     public static void main(String[] args) {
         FileReader fr = null;
 
@@ -27,9 +42,10 @@ public class LecturaJSON {
             JsonArray jsonArray = jsonElem.getAsJsonArray();
             Iterator<JsonElement> iterator = jsonArray.iterator();
 
-            System.out.println(ANSI_BLUE + "\nLEYENDO CONTACTOS DEL FICHERO '" + ruta + "'" + ANSI_RESET);
-            System.out.println(ANSI_BLUE + "----------------------------------------------\n" + ANSI_RESET);
+            System.out.println(ANSI_CYAN + "\nLEYENDO CONTACTOS DEL FICHERO '" + ruta + "'" + ANSI_RESET);
+            System.out.println(ANSI_CYAN + "----------------------------------------------\n" + ANSI_RESET);
 
+            //Recorrer todos los contactos y mostrarlos en consola
             while (iterator.hasNext()) {
                 JsonElement jsonElem2 = iterator.next();
                 JsonObject jsonObj = jsonElem2.getAsJsonObject();
@@ -42,23 +58,26 @@ public class LecturaJSON {
                 System.out.println("Ciudad: " + iterator2.next().getValue());
                 System.out.println("País: " + iterator2.next().getValue());
                 System.out.println(ANSI_GREEN + "\n-----------------------------------\n" + ANSI_RESET);
-
-                
             }
+            
+         System.out.println(ANSI_CYAN + "\n-----------------------------------\nLectura finalizada\n" + ANSI_RESET);
         } catch (FileNotFoundException e) {
+            System.err.println(ANSI_RED);
             e.printStackTrace();
-        } catch (JsonSyntaxException e) {
+            System.err.println(ANSI_RESET);
+        } catch (JsonSyntaxException | IllegalStateException e) {
+            System.err.println(ANSI_RED);
             e.printStackTrace();
-        } catch (IllegalStateException e){
-            e.printStackTrace();
-        } finally{
+            System.err.println(ANSI_RESET);
+        } finally {
             try {
-                if (fr != null){
-                    fr.close();
-                }
+                if (fr != null) fr.close();
             } catch (IOException e) {
+                System.err.println(ANSI_RED);
                 e.printStackTrace();
+                System.err.println(ANSI_RESET);
             }
         }
     }
 }
+
